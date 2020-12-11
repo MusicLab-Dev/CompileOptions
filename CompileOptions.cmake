cbd94c2267f81112dcd98fad3e310f16fbc13129
MatthieuMv <m.moinvaziri@gmail.com>
2020-12-11 17:57:34 +0100

    Updated VCPKG compatibility

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -4,6 +4,18 @@ get_filename_component(CompileOptionsRoot ${CMAKE_CURRENT_LIST_FILE} PATH)
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+# Search for a triplet in environment if not explicitly set
+if(NOT DEFINED DVCPKG_TARGET_TRIPLET AND DEFINED ENV{DVCPKG_TARGET_TRIPLET})
+    message("DVCPKG_TARGET_TRIPLET found in environment: " $ENV{DVCPKG_TARGET_TRIPLET})
+    set(DVCPKG_TARGET_TRIPLET $ENV{DVCPKG_TARGET_TRIPLET})
+endif()
+
+# Search for a toolchain in environment if not explicitly set
+if(NOT DEFINED CMAKE_TOOLCHAIN_FILE AND DEFINED ENV{CMAKE_TOOLCHAIN_FILE})
+    message("CMAKE_TOOLCHAIN_FILE found in environment: " $ENV{CMAKE_TOOLCHAIN_FILE})
+    set(CMAKE_TOOLCHAIN_FILE $ENV{CMAKE_TOOLCHAIN_FILE})
+endif()
+
 # Add compiler specific flags
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     add_compile_options(
