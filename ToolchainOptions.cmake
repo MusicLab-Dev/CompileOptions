bfddc27664a763ab2c3cf5a8e251dfe28be8cca8
unknown <m.moinvaziri@gmail.com>
2021-11-20 14:26:37 +0100

    Updated vcpkg

--- ToolchainOptions.cmake
+++ ToolchainOptions.cmake
@@ -2,8 +2,8 @@ get_filename_component(ToolchainOptionsRoot ${CMAKE_CURRENT_LIST_FILE} PATH)
 
 # Search for a triplet in environment if not explicitly set
 if(NOT DEFINED VCPKG_TARGET_TRIPLET AND DEFINED ENV{VCPKG_TARGET_TRIPLET})
-    message("DVCPKG_TARGET_TRIPLET found in environment: " $ENV{VCPKG_TARGET_TRIPLET})
-    set(DVCPKG_TARGET_TRIPLET $ENV{VCPKG_TARGET_TRIPLET})
+    message("VCPKG_TARGET_TRIPLET found in environment: " $ENV{VCPKG_TARGET_TRIPLET})
+    set(VCPKG_TARGET_TRIPLET $ENV{VCPKG_TARGET_TRIPLET})
 endif()
 
 # Search for a toolchain in environment if not explicitly set
